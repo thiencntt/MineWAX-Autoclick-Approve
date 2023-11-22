b22b7874c5d25378e53f7a4402ba3700c327da78
thiencntt <nguyenchithien@gmail.com>
2023-11-22 22:28:40 +0700

    Upgrade 1.2

--- dungeon.js
+++ dungeon.js
@@ -26,6 +26,24 @@ setInterval(() => {
         if (claimLevelBTN) {
             claimLevelBTN.click()
         }
+        let errA = document.querySelector("#app > div.popup.error-view > div > div > button > span")
+        if (errA) {
+            errA.click()
+        }
+        let daclaim = document.querySelector("#app > div.popup.error-view > div > div > button")
+        if (daclaim) {
+            daclaim.click()
+        }
+        let btnproceed = document.querySelector("#app > div.base-layout.base-layout--green-bg > div.base-layout__main > div.popup.not-enough-stamina-popup > div > div > button.btn.not-enough-stamina-popup__wrapper-btn-proceed.btn--primary.btn--primary-without-arrow")
+        if (btnproceed) {
+            btnproceed.click()
+        }
+        let nocandy = document.querySelector("#app > div.base-layout.base-layout--green-bg > div.base-layout__main > div.popup > div > div > button.btn.btn--outlined.confirm-send-wombat-with-claimable-candy-modal__btn.confirm-send-wombat-with-claimable-candy-modal__btn--reject")
+        if (nocandy) {
+            nocandy.click()
+            
+        }
+    
     }
     
 }, 1000)
